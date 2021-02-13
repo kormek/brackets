c4d147fb0c1638e504b062bd616e8d11277e408e
Vladimir <anikol200@gmail.com>
2021-02-13 21:07:40 +0300

    fix: 17/20 tests are working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let mystack = []; 
+  let open = [];
+   let close = [];
+  str = str.split('');
+  if(str.length%2) return false;
+  for(let i =0;i<bracketsConfig.length;i++){
+    open.push(bracketsConfig[i][0]);
+     close.push(bracketsConfig[i][1]);
+  }
+  
+  for(let i = 0;i<str.length;i++){
+     
+    if(open.includes(str[i])) {
+     
+      mystack.push(str[i]);
+      
+    } if(close.includes(str[i]) ) 
+      {
+       
+       if(close[open.indexOf(mystack[mystack.length-1])] === str[i]  ) { mystack.pop();
+      } else {
+        
+        return false;
+      }
+    }
+
+  }
+
+  return true;
 }
